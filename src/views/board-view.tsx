c0b73bbcf98b28a026c451ef33a80c4818103ef1
dajhelyan <assiaxramx10@gmail.com>
2023-03-08 19:19:08 -0500

    Add query mobile to board view

--- src/views/board-view.tsx
+++ src/views/board-view.tsx
@@ -7,45 +7,52 @@ import React from "react";
 import Filter from "../components/filter/filter";
 import Heading from "../components/heading/heading";
 import Search from "../components/search-component/search";
-import StatusBoard from "../components/status-board/status-board";
+import StatusColumn from "../components/status-task-column/status-column";
+import StatusBoard from "../components/task-list/task-list";
 
 const useStyles = makeStyles({
   boardLayout: {
     width: "100%",
-    ...shorthands.padding("20px"),
+    ...shorthands.margin("0.5em", "2.5em", "1em", "2.5em"),
   },
   title: {
     ...shorthands.margin("20px", "20px"),
   },
   flex: {
     display: "flex",
-    flexWrap: "wrap",
     justifyContent: "space-between",
+    '@media (max-width: 767.98px)':{
+      maxWidth: "100%",
+      flexWrap: "wrap",
+    }
   },
   w100: {
     width: "100%",
   },
-
-  m10: {
-    ...shorthands.margin("0px", "0px", "1rem", "0px"),
-  }
+  main: {
+    width: "100%",
+    '@media (max-width: 767.98px)':{
+      width: "100%",
+      ...shorthands.margin("1em"),
+    }
+  },
 });
 
 export default function BoardView() {
   const styles = useStyles();
   return (
-    <div className={styles.boardLayout}>
-      <div className={styles.m10}>
+    <div className={styles.boardLayout} >
+      <div >
         <Heading ></Heading>
       </div>
       
-      <div className={mergeClasses(styles.flex, styles.m10)}>
+      <div className={mergeClasses(styles.flex)}>
         <Search></Search>
         <Filter></Filter>
       </div>
-      <div>
-        <StatusBoard></StatusBoard>
+      <div >
+        <StatusColumn></StatusColumn>
       </div>
     </div>
   );
-}
+} 
