06cc981b3b70cb36ac6b3b1a2c5910f445658aeb
dajhelyan <assiaxramx10@gmail.com>
2023-03-31 15:59:24 -0500

    Add TaskStatus type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,30 @@
+import { EnumType } from "typescript"
+import { ColumnStatusEnum } from "./components/enums"
+
 export interface Task {
-  id: number,
+  id: any,
   titleTask: string,
   description: string,
   responsable: string,
   date: string,
   priority: string,
-  status: string
+  status: ColumnStatusEnum
+}
+
+export interface TaskStatus {
+  pending: Status,
+  inProgress: Status,
+  done: Status
+}
+
+export interface Status {
+  title: string,
+  slug: string
+  color: string
+}
+
+export interface StatusColor{
+  informative: string
+  warning: string
+  success: string
 }
